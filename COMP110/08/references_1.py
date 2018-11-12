f4845bedec2c49ec318ee29044d8697342067abb
Ed Powley <edpowley@gmail.com>
2018-11-12 00:11:20 +0000

    COMP110 session 8

--- COMP110/08/references_1.py
+++
@@ -1,5 +0,0 @@
-a = 10
-b = a
-a = 20
-print "a:", a
-print "b:", b
--- COMP110/08/square_root.py
+++
@@ -1,6 +0,0 @@
-square_root = {}
-for i in xrange(30):
-    square_root[i*i] = i
-
-for key, value in square_root.iteritems():
-    print "The square root of", key, "is", value
