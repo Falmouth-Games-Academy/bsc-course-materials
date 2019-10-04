64f1af95785fc9fec2a582503d0929b2f439a679
Unknown <gareth_lewis@yahoo.com>
2019-10-04 16:21:53 +0100

    COMP280L5
    unreal http project

---
+++ COMP280/05/code/test00/Source/test00/MyHTTPRequest.cpp
@@ -0,0 +1,127 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyHTTPRequest.h"
+
+// Sets default values
+AMyHTTPRequest::AMyHTTPRequest()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyHTTPRequest::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Http = &FHttpModule::Get();
+
+	// You can uncomment this out for testing.
+	//FRequest_Login LoginCredentials;
+	//LoginCredentials.email = TEXT("asdf@asdf.com");
+	//LoginCredentials.password = TEXT("asdfasdf");
+	//Login(LoginCredentials);
+	
+}
+
+// Called every frame
+void AMyHTTPRequest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
+/**************************************************************************************************************************/
+
+
+
+
+TSharedRef<IHttpRequest> AMyHTTPRequest::RequestWithRoute(FString Subroute) {
+	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
+	Request->SetURL(ApiBaseUrl + Subroute);
+	SetRequestHeaders(Request);
+	return Request;
+}
+
+void AMyHTTPRequest::SetRequestHeaders(TSharedRef<IHttpRequest>& Request) {
+	Request->SetHeader(TEXT("User-Agent"), TEXT("X-UnrealEngine-Agent"));
+	Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	Request->SetHeader(TEXT("Accepts"), TEXT("application/json"));
+}
+
+TSharedRef<IHttpRequest> AMyHTTPRequest::GetRequest(FString Subroute) {
+	TSharedRef<IHttpRequest> Request = RequestWithRoute(Subroute);
+	Request->SetVerb("GET");
+	return Request;
+}
+
+TSharedRef<IHttpRequest> AMyHTTPRequest::PostRequest(FString Subroute, FString ContentJsonString) {
+	TSharedRef<IHttpRequest> Request = RequestWithRoute(Subroute);
+	Request->SetVerb("POST");
+	Request->SetContentAsString(ContentJsonString);
+	return Request;
+}
+
+void AMyHTTPRequest::Send(TSharedRef<IHttpRequest>& Request) {
+	Request->ProcessRequest();
+}
+
+bool AMyHTTPRequest::ResponseIsValid(FHttpResponsePtr Response, bool bWasSuccessful) {
+	if (!bWasSuccessful || !Response.IsValid()) return false;
+	if (EHttpResponseCodes::IsOk(Response->GetResponseCode())) return true;
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Http Response returned error code: %d"), Response->GetResponseCode());
+		return false;
+	}
+}
+
+void AMyHTTPRequest::SetAuthorizationHash(FString Hash, TSharedRef<IHttpRequest>& Request) {
+	Request->SetHeader(AuthorizationHeader, Hash);
+}
+
+
+
+/**************************************************************************************************************************/
+
+
+
+template <typename StructType>
+void AMyHTTPRequest::GetJsonStringFromStruct(StructType FilledStruct, FString& StringOutput) {
+	FJsonObjectConverter::UStructToJsonObjectString(StructType::StaticStruct(), &FilledStruct, StringOutput, 0, 0);
+}
+
+template <typename StructType>
+void AMyHTTPRequest::GetStructFromJsonString(FHttpResponsePtr Response, StructType& StructOutput) {
+	StructType StructData;
+	FString JsonString = Response->GetContentAsString();
+	FJsonObjectConverter::JsonObjectStringToUStruct<StructType>(JsonString, &StructOutput, 0, 0);
+}
+
+
+
+/**************************************************************************************************************************/
+
+
+
+void AMyHTTPRequest::Login(FRequest_Login LoginCredentials) {
+	FString ContentJsonString;
+	GetJsonStringFromStruct<FRequest_Login>(LoginCredentials, ContentJsonString);
+
+	TSharedRef<IHttpRequest> Request = PostRequest("user/login", ContentJsonString);
+	Request->OnProcessRequestComplete().BindUObject(this, &AMyHTTPRequest::LoginResponse);
+	Send(Request);
+}
+
+void AMyHTTPRequest::LoginResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
+	if (!ResponseIsValid(Response, bWasSuccessful)) return;
+
+	FResponse_Login LoginResponse;
+	GetStructFromJsonString<FResponse_Login>(Response, LoginResponse);
+
+	UE_LOG(LogTemp, Warning, TEXT("Id is: %d"), LoginResponse.id);
+	UE_LOG(LogTemp, Warning, TEXT("Name is: %s"), *LoginResponse.name);
+}
+
