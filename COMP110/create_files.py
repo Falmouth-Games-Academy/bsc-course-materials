81077fd05c737f48ef561fc60a7dd646a77a3edc
edpowley <edpowley@gmail.com>
2019-10-06 20:22:05 +0100

    Created stub files

---
+++ COMP110/create_files.py
@@ -0,0 +1,32 @@
+﻿import os
+
+for i in range(3, 12+1):
+	for session in ["lecture", "workshop"]:
+		filename_base = os.path.join("%02i" % i, "2019-20-COMP110-%02i-%s-materials" % (i, session))
+		
+		with open(filename_base + ".tex", "wt") as f:
+			f.writelines([
+				r"\documentclass[handout, xcolor={dvipsnames}]{beamer}\usepackage{etoolbox}\newtoggle{printable}\togglefalse{printable}",
+				r"\input{%s_main_body}" % session,
+				""
+			])
+		
+		with open(filename_base + "-screen.tex", "wt") as f:
+			f.writelines([
+				r"\documentclass[xcolor={dvipsnames}]{beamer}\usepackage{etoolbox}\newtoggle{printable}\togglefalse{printable}",
+				r"\input{%s_main_body}" % session,
+				""
+			])
+		
+		with open(filename_base + "-screen.tcp", "wt") as f:
+			f.write("""[FormatInfo]
+Type=TeXnicCenterProjectInformation
+Version=4
+
+[ProjectInfo]
+MainFile=2019-20-COMP110-%02i-%s-materials-screen.tex
+UseBibTeX=0
+UseMakeIndex=0
+ProjectLanguage=en
+ProjectDialect=US
+""" % (i, session))
