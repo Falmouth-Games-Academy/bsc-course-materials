b592f1a4af99f771450b00614ad22ab070647dd5
edpowley <edpowley@gmail.com>
2019-10-20 19:49:23 +0100

    COMP110 workshop 5

---
+++ COMP110/05/time_list_creation.py
@@ -0,0 +1,22 @@
+# Time creation of a list of random elements
+# Write the results to a CSV file
+
+import time
+import random
+
+rep_count = 10
+
+with open("results.csv", "wt") as f:
+	for n in range(10, 10000, 10):
+		print(n)
+
+		start_time = time.clock()
+		for repetition in range(rep_count):
+			# Create a list of size n
+			my_list = []
+			for i in range(n):
+				my_list.append(random.randrange(1000))
+
+		end_time = time.clock()
+		total_time = end_time - start_time
+		f.write("{0},{1}\n".format(n, total_time))
