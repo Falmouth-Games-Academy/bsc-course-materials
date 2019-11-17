f342762f5e6febb68c13c3f50d172b9273e84bf3
Ed Powley <edpowley@gmail.com>
2019-11-17 22:27:53 +0000

    Started COMP110 09

---
+++ COMP110/09/TestProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TestProject
+{
+    class Pair<ElementType>
+    {
+        public ElementType first;
+        public ElementType second;
+
+        public Pair(ElementType f, ElementType s)
+        {
+            first = f;
+            second = s;
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var p = new Pair<string>("hello", "world");
+            Console.WriteLine("{0}, {1}", p.first, p.second);
+        }
+    }
+}
