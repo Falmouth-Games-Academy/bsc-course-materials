08d5e0416c4496ca1a01fc71dcad7896fe6ba83f
Ed Powley <edpowley@gmail.com>
2019-11-18 04:03:12 +0000

    COMP110 workshop 9

--- COMP110/09/TestProject/Program.cs
+++ COMP110/09/TestProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TestProject
 {
@@ -14,12 +15,55 @@ namespace TestProject
         }
     }
 
+    struct Foo
+    {
+        public string a;
+        public int b;
+    }
+
     class MainClass
     {
         public static void Main(string[] args)
         {
+            var q = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+
             var p = new Pair<string>("hello", "world");
             Console.WriteLine("{0}, {1}", p.first, p.second);
+
+            int[,,,,] a = new int[10, 10, 10, 10, 10];
+
+            Console.WriteLine("{0}", a);
+
+            var myList = new List<int>();
+
+            string s = "Hello";
+            s += "world";
+
+            Dictionary<List<int>, string> d = new Dictionary<List<int>, string>();
+
+            var age = new Dictionary<string, int>
+            {
+                ["Alice"] = 23,
+                ["Bob"] = 36,
+                ["Charlie"] = 27
+            };
+
+            Console.WriteLine(age["Alice"]);  // prints 23
+            age["Bob"] = 40;     // overwriting an existing item
+            age.Add("Emily", 29);
+            age["Denise"] = 21;  // adding a new item      
+
+            foreach (var kv in age)
+            {
+                Console.WriteLine("{0} is {1} years old", kv.Key, kv.Value);
+            }
+
+            Dictionary<int, int> sqrt = new Dictionary<int, int>();
+            for (int i = 0; i < 30; i++)
+                sqrt[i * i] = i;
+
+            foreach (var kv in sqrt)
+                Console.WriteLine("{0} {1}", kv.Key, kv.Value);
         }
     }
 }
