e520abe42daa13ebe71179203ce6e9d8204bb46e
Ed Powley <edpowley@gmail.com>
2020-11-08 22:41:03 +0000

    Updtes to COMP110 08

---
+++ COMP110/08/references_3.cs
@@ -0,0 +1,15 @@
+class Foo
+{
+    public int value;
+
+    public Foo(int v)
+    {
+        value = v;
+    }
+}
+
+Foo a = new Foo(10);
+Foo b = new Foo(10);
+a.value = 20
+Console.WriteLine($"a: {a.value}");
+Console.WriteLine($"b: {b.value}");
